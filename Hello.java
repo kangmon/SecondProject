643407b2b58c4e75672940e70a62aeaf8294ff7b
kangmon <kangmon@naver.com>
2015-12-28 09:57:51 +0900

    Hello.java modification on Local Repository

--- Hello.java
+++ Hello.java
@@ -1,8 +1,9 @@
+//For command line git tutorial
+//Local Repository Modification
 public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello Git!");
-		System.out.println("What is Git?")
-;	
+		System.out.println("What is Git?");	
 		System.out.println("That's DVSC"); //my area		
 		System.out.println("2002 BitKeepper"); //branch area
 
