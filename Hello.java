ff0c5a2c5113ee36fceea5f0d6a67af26232a90a
kangmon <kangmon@naver.com>
2015-12-29 09:26:56 +0900

    GitHub for git pull

--- Hello.java
+++ Hello.java
@@ -7,6 +7,7 @@ public class Hello {
 		System.out.println("What is Git?");	
 		System.out.println("That's DVSC"); //my area		
 		System.out.println("2002 BitKeepper"); //branch area
+		System.out.println("That's version control"); // in git hub
 
 	}
 }
